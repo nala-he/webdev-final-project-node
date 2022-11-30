e093bc15e82b1fdf78d98ba371c9eca41b3bd383
MaceeQi <maceeqi@gmail.com>
2022-11-30 12:07:35 -0500

    implement fridge ingredients schema

--- controllers/fridge-ingredients/fridge-ingredients-schema.js
+++ controllers/fridge-ingredients/fridge-ingredients-schema.js
@@ -1,1 +1,9 @@
-import mongoose from "mongoose";
+import mongoose, {Schema} from "mongoose";
+
+const fridgeIngredientsSchema = mongoose.Schema({
+    checked: {type: Boolean, default: false},
+    title: {type: String, required: true},
+    user: {type: Schema.Types.ObjectId, ref: "UserModel", required: true},
+}, {collection: "fridge-ingredients"});
+
+export default fridgeIngredientsSchema;
