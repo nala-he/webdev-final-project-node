8919ccd4a44b2cf8efaab29d7140edc56dc9ac7a
MaceeQi <maceeqi@gmail.com>
2022-11-30 12:03:16 -0500

    update saved recipes schema requirements

--- controllers/saved-recipes/saved-recipes-schema.js
+++ controllers/saved-recipes/saved-recipes-schema.js
@@ -1,8 +1,8 @@
 import mongoose, {Schema} from "mongoose";
 
 const savedRecipesSchema = mongoose.Schema({
-    savedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
-    recipe: {type: Schema.Types.ObjectId, ref: "RecipeModel"},
+    savedBy: {type: Schema.Types.ObjectId, ref: "UserModel", required: true},
+    recipe: {type: Schema.Types.ObjectId, ref: "RecipeModel", required: true},
 }, {collection: "saved-recipes"});
 
 export default savedRecipesSchema;
