41883bc386cec46e1b8746bcdf0d3966441ec298
MaceeQi <maceeqi@gmail.com>
2022-11-30 13:55:14 -0500

    update fridge ingredients schema to reflect correct name for users model

--- controllers/fridge-ingredients/fridge-ingredients-schema.js
+++ controllers/fridge-ingredients/fridge-ingredients-schema.js
@@ -3,7 +3,7 @@ import mongoose, {Schema} from "mongoose";
 const fridgeIngredientsSchema = mongoose.Schema({
     checked: {type: Boolean, default: false},
     title: {type: String, required: true},
-    user: {type: Schema.Types.ObjectId, ref: "UserModel", required: true},
+    user: {type: Schema.Types.ObjectId, ref: "UsersModel", required: true},
 }, {collection: "fridge-ingredients"});
 
 export default fridgeIngredientsSchema;
