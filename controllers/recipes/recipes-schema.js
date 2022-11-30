fa2fcda5a24946dd77ffa1e8e008ace90616f80f
Yena <t1yena@gmail.com>
2022-11-30 18:41:18 -0500

    added recipes model and schema

--- controllers/recipes/recipes-schema.js
+++ controllers/recipes/recipes-schema.js
@@ -2,7 +2,20 @@ import mongoose from 'mongoose';
 
 const RecipesSchema = mongoose.Schema({
     authorId: {type: Schema.Types.ObjectId, ref: "UserModel"},
-
+    author: String,
+    avatar: String,
+    dishname: {type: String, required: true},
+    intro: {type: String, required: true},
+    recipePic: {type: String, default: "emptyRecipe.jpg"},
+    prepTime: {type: String, required: true},
+    cookTime: {type: String, required: true},
+    totalTime: {type: String, required: true},
+    servings: {type: Number, required: true},
+    difficulty: {type: String, required: true},
+    calories: {type: String, required: true},
+    fat: {type: String, required: true},
+    carbs: {type: String, required: true},
+    protein: {type: String, required: true}
 }, {collection:"recipes"});
 
 export default RecipesSchema;
