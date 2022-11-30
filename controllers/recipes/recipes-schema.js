842f2a3847eb7046a6a0523562f84ff83f5022a4
Yena <t1yena@gmail.com>
2022-11-30 17:42:09 -0500

    added authorid

--- controllers/recipes/recipes-schema.js
+++ controllers/recipes/recipes-schema.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
 const RecipesSchema = mongoose.Schema({
+    authorId: {type: Schema.Types.ObjectId, ref: "UserModel"},
+
 }, {collection:"recipes"});
 
 export default RecipesSchema;
