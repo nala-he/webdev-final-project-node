977fe612fdefed23380acd46702a2ea217c32dc1
Yena <t1yena@gmail.com>
2022-12-01 16:14:33 -0500

    fixed typo for recipesmodel

--- controllers/recipe-ingredients/recipe-ingredients-schema.js
+++ controllers/recipe-ingredients/recipe-ingredients-schema.js
@@ -1,7 +1,7 @@
 import mongoose, {Schema} from 'mongoose';
 
 const RecipeIngredientsSchema = mongoose.Schema({
-    rid: {type: Schema.Types.ObjectId, ref: "RecipeModel",required: true},
+    rid: {type: Schema.Types.ObjectId, ref: "RecipesModel",required: true},
     ingredient: {type: String, required: true},
 }, {collection:"recipeIngredients"});
 
