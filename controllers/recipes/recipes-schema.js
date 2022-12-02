361cd002df79d8743ec0f455bb36a12bb51f5002
Yena <t1yena@gmail.com>
2022-12-02 01:49:43 -0500

    change recipe schema default to undefined

--- controllers/recipes/recipes-schema.js
+++ controllers/recipes/recipes-schema.js
@@ -2,18 +2,18 @@ import mongoose, {Schema} from 'mongoose';
 
 const RecipesSchema = mongoose.Schema({
     authorId: {type: Schema.Types.ObjectId, ref: "UsersModel"},
-    dishName: {type: String, default:null},
-    intro: {type: String, default:null},
+    dishName: {type: String, default:undefined},
+    intro: {type: String, default:undefined},
     recipePic: {type: String, default: "emptyRecipe.jpg"},
-    prepTime: {type: String, default:null},
-    cookTime: {type: String, default:null},
-    totalTime: {type: String, default:null},
-    servings: {type: Number, default:null},
-    difficulty: {type: String, default:null},
-    calories: {type: Number, default:null},
-    fat: {type: String, default:null},
-    carbs: {type: String, default:null},
-    protein: {type: String, default:null},
+    prepTime: {type: String, default:undefined},
+    cookTime: {type: String, default:undefined},
+    totalTime: {type: String, default:undefined},
+    servings: {type: Number, default:undefined},
+    difficulty: {type: String, default:undefined},
+    calories: {type: Number, default:undefined},
+    fat: {type: String, default:undefined},
+    carbs: {type: String, default:undefined},
+    protein: {type: String, default:undefined},
     privacy: {type: String, enum: ['PUBLIC', 'PRIVATE'], default: 'PUBLIC'}
 }, {collection:"recipes"});
 
