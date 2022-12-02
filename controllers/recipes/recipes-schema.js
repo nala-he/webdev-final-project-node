baa27aac3dc5a2a5fea3080cd1980994d92d46cf
Yena <t1yena@gmail.com>
2022-12-01 20:38:21 -0500

    edit schema to have empty default values when recipe is created

--- controllers/recipes/recipes-schema.js
+++ controllers/recipes/recipes-schema.js
@@ -2,18 +2,18 @@ import mongoose, {Schema} from 'mongoose';
 
 const RecipesSchema = mongoose.Schema({
     authorId: {type: Schema.Types.ObjectId, ref: "UsersModel",required: true},
-    dishName: {type: String, required: true},
-    intro: {type: String, required: true},
+    dishName: {type: String, required: true, default:''},
+    intro: {type: String, required: true, default:''},
     recipePic: {type: String, default: "emptyRecipe.jpg"},
-    prepTime: {type: String, required: true},
-    cookTime: {type: String, required: true},
-    totalTime: {type: String, required: true},
-    servings: {type: Number, required: true},
-    difficulty: {type: String, required: true},
-    calories: {type: Number, required: true},
-    fat: {type: String, required: true},
-    carbs: {type: String, required: true},
-    protein: {type: String, required: true},
+    prepTime: {type: String, required: true, default:''},
+    cookTime: {type: String, required: true, default:''},
+    totalTime: {type: String, required: true, default:''},
+    servings: {type: Number, required: true, default:''},
+    difficulty: {type: String, required: true, default:''},
+    calories: {type: Number, required: true, default:''},
+    fat: {type: String, required: true, default:''},
+    carbs: {type: String, required: true, default:''},
+    protein: {type: String, required: true, default:''},
     privacy: {type: String, enum: ['PUBLIC', 'PRIVATE'], default: 'PUBLIC'}
 }, {collection:"recipes"});
 
